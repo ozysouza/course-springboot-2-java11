ba73388d1873b25d58962e98629bb8daf54c44de
Oziel Souza <ozysouzaa@gmail.com>
2020-12-31 16:28:42 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/ozielsouza/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ozielsouza.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ozielsouza.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
