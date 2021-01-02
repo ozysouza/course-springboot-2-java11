8c9f65c46fb9246d97a9761c2736d3c8004faa06
Oziel Souza <ozysouzaa@gmail.com>
2021-01-01 21:47:11 -0300

    Service layer and component registration

--- src/main/java/com/ozielsouza/course/resources/UserResource.java
+++ src/main/java/com/ozielsouza/course/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.ozielsouza.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ozielsouza.course.entities.User;
+import com.ozielsouza.course.services.UserService;
 
 @RestController  //Para implementar um recurso web
 @RequestMapping(value = "/users") //Inclusão de nome para o recurso
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Oziel", "ozysouza@gmail.com", "865974697", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
